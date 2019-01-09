7fa5a9c2c50f7957216d4a23d2a8e2801dc56902
ykrueng <yasirah.krueng@gmail.com>
2019-01-09 15:23:32 -0600

    add GET /api/users/:id/posts

--- server/server.js
+++ server/server.js
@@ -43,4 +43,30 @@ server.get("/api/users/:id", async (req, res) => {
   }
 });
 
+// route handler for GET /api/users/:id/post
+server.get("/api/users/:id/posts",
+  async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+      res.status(400).json({
+        message: "please provide an id"
+      });
+    }
+
+    try {
+      const user = await userDb.get(id);
+      if (!user) {
+        res.status(404).json({
+          message: "user does not exist"
+        })
+      }
+      const posts = await userDb.getUserPosts(id);
+      res.status(200).json(posts);
+    } catch(err) {
+      res.status(500).json({
+        message: "cannot retrieve user posts"
+      })
+    }
+});
+
 module.exports = server;
